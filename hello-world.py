cd36199d81b73ef1f421efe9036853bc602d152f
Jasmin Lamb <jasmin.lamb@studmail.htw-aalen.de>
2026-03-29 17:28:24 +0200

    Add hello world program

---
+++ hello-world.py
@@ -0,0 +1,5 @@
+def greet_user(name: str) -> str:
+    return f"Hallo, {name}! Willkommen bei Software Engineering."
+if __name__ == "__main__":
+    user_name = input("Geben Sie Ihren Namen ein: ")
+    print(greet_user(user_name))
